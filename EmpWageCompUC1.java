d81d2c987e4ba326fd91cb4e2400bb398cc7fa5d
Trishyo <elnino.trishyo@gmail.com>
2022-07-19 19:41:49 +0530

    Added : EmployeeWage UseCase1

---
+++ EmpWageCompUC1.java
@@ -0,0 +1,11 @@
+package EmpWageComp;
+
+public class EmpWageCompUC1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+	}
+
+}
